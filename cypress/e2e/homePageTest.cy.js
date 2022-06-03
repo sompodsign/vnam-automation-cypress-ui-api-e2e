6025beb714496f9874ab93c16245fd200b94d1ed
Shampad Sharkar <sompodsign@gmail.com>
2022-06-03 17:05:11 +0600

    homepage testcases

--- cypress/e2e/homePageTest.cy.js
+++ cypress/e2e/homePageTest.cy.js
@@ -1,13 +1,46 @@
 import HomePage from "../pageObjects/homePage";
+import "cypress-localstorage-commands";
 
 
 describe('User login tests', () => {
 
     const homePage = new HomePage();
 
+    let testData;
+
+
+    beforeEach(() => {
+      cy.fixture('login').then(dataJson => {
+        testData = dataJson;
+      });
+      cy.setLocalStorage("isLyceumAuthenticated", true);
+
+    });
+
+
     it('UI Test 1.1 - User visits homepage', () => {
-        homePage.getHomePage();
-
-
+            homePage.getHomePage(testData.passPhrase);
     })
+
+    it('UI Test 1.2 - Nami logo on top left of the page', () => {
+        homePage.checkNamiLogo();
+    })
+    
+    it('UI Test 1.3 - Connect wallet button is visible', () => {
+        homePage.checkConnectWalletButton();
+    });
+
+    it('UI Test 1.4 - Filters menu is visible', () => {
+        homePage.checkFiltersMenu();
+    });
+
+    it('UI Test 1.5 - Check user can search for a game', () => {
+        homePage.checkGameSearch();
+    });
+
+    it('UI Test 1.6 - Check game detail page', () => {
+        homePage.checkGameDetailPage();
+    });
+
+
 })
--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -1,11 +1,53 @@
 import "cypress-iframe";
+import "cypress-xpath";
+
 
 class Homepage {
 
-    getHomePage = () => {
-        cy.visit("https://qa.app.nami.gg/games")
 
+
+    // getHomePage = (phrase) => {
+    //     cy.visit("https://qa.app.nami.gg/games", {
+    //         onBeforeLoad: (win) => {
+    //             cy.stub(win, 'prompt').returns(phrase);
+    //         }
+    //     });
+    // }
+    getHomePage = (phrase) => {
+        cy.visit("https://qa.app.nami.gg/games");
     }
+
+    // Check nami logo
+    checkNamiLogo = () => {
+        cy.get('[alt="Lyceum"]').should('be.visible');
+    }
+
+    // check connect wallet button
+    checkConnectWalletButton = () => {
+        //get button with text
+        cy.contains('Connect Wallet').should('be.visible');
+    }
+
+    // check filters menu on the page
+    checkFiltersMenu = () => {
+        cy.get('.css-h8vjxy').should('be.visible');
+    }
+
+    checkGameSearch = () => {
+        cy.get('[placeholder="Search games"]').should('be.visible');
+        cy.get('[placeholder="Search games"]').type('Test Game 1');
+        cy.contains('Test Game 1').should('be.visible');
+        cy.get('[placeholder="Search games"]').clear();
+    }
+
+    checkGameDetailPage = () => {
+        cy.contains('Test Game 1').click();
+        cy.contains('Test Game 1').should('be.visible');
+        cy.xpath('//h4[contains(text(),"Test Game 1")]').should('be.visible');
+        cy.go('back');
+    }
+
+
 }
 
 export default Homepage;
